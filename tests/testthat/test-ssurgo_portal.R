494d8174c8479e7d1bf226885afcd0177afc274c
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-04-03 16:09:37 -0700

    Add minimal tests (gpkg template only)

---
+++ tests/testthat/test-ssurgo_portal.R
@@ -0,0 +1,91 @@
+test_that("ssurgo_portal() works (template only, no SSURGO data)", {
+
+  # test helper functions
+  .expect_status_isTRUE <- function(x) {
+    expect_true(inherits(x, 'list') &&
+                 !is.null(x$status) &&
+                 isTRUE(x$status))
+  }
+
+  .expect_response <- function(x) {
+    expect_true(inherits(x, 'list') &&
+                 !is.null(x$response) &&
+                 is.list(x$response))
+  }
+
+  .expect_nodes <- function(x) {
+    expect_true(inherits(x, 'list') &&
+                 !is.null(x$nodes) &&
+                 is.list(x$nodes))
+  }
+
+  # check if its alive
+  .expect_status_isTRUE(ssurgo_portal("getstatus"))
+  .expect_response(ssurgo_portal("getstatus", schema = TRUE))
+
+  # sample subdirectory
+  dir.create("foo")
+
+  # inspect folder tree
+  .expect_nodes(ssurgo_portal(
+    "getfoldertree",
+    path = ".",
+    folderpattern = ".*",
+    filepattern = "txt",
+    ignorefoldercase = TRUE,
+    ignorefilecase = TRUE,
+    showfiles = FALSE,
+    maxdepth = 2
+  ))
+
+  # get template catalog
+  .expect_status_isTRUE(ssurgo_portal("gettemplatecatalog"))
+
+  # create empty geopackage
+  .expect_status_isTRUE(ssurgo_portal(
+    "copytemplate",
+    templatename = "GeoPackage",
+    folder = "foo",
+    filename = "my_gpkg",
+    overwrite = TRUE
+  ))
+
+  dbp <- file.path("foo", "my_gpkg.gpkg")
+
+  .expect_status_isTRUE(ssurgo_portal("opentemplate", database = dbp))
+
+  # one subfolder that does not pass pretest
+  dbt <- ssurgo_portal(
+    "pretestimportcandidates",
+    database = dbp,
+    root = ".",
+    istabularonly = FALSE
+  )
+  .expect_status_isTRUE(dbt)
+  expect_true(isFALSE(dbt$allpassed))
+  expect_equal(length(dbt$subfolders), 1)
+
+  # can't import a subfolder that does not exist
+  expect_output(ssurgo_portal(
+    "importcandidates",
+    database = dbp,
+    root = ".",
+    istabularonly = FALSE,
+    skippretest = TRUE,
+    subfolders = list("WV999"),
+    loadinspatialorder = FALSE,
+    loadspatialdatawithinsubprocess = TRUE,
+    dissolvemupolygon = TRUE
+  ), "sacatlog.*does not exist")
+
+  .expect_status_isTRUE(ssurgo_portal("getdatabaseinventory", database = dbp))
+
+  # TODO: does this do anything ?? status is TRUE anyway
+  .expect_status_isTRUE(ssurgo_portal(
+    "deleteareasymbols",
+    database = dbp,
+    areasymbols = list("somethingdifferent")
+  ))
+
+  unlink("foo", recursive = TRUE)
+})
